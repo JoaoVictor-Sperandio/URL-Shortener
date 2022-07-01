05f0041bffc30b3185fd23879becd274325efd0e
João Victor Alonso de Paula Sperandio <joaovictorapsperandio@gmail.com>
2022-07-01 15:01:03 -0300

    Delete Constants.ts

--- src/config/Constants.ts
+++
@@ -1,5 +0,0 @@
-export const config = {
-    API_URL: "http://localhost:5000",
-    MONGO_CONNECTION:
-    'mongodb+srv://Sperandio:u9qNJx3LRUnBWRJW@urlshortener.bcuswgi.mongodb.net/?retryWrites=true&w=majority',
-}
